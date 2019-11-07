851087d85fc2e0a5a7fa8e53ec436387fcfb04f5
Danielle Barnas <danielle.barnas@csun.edu>
2019-11-06 16:53:03 -0800

    Tidy Apex Data Log csv - initial commit

---
+++ Outlog_Data_Tidy.R
@@ -0,0 +1,32 @@
+rm(list=ls())
+library(tidyverse)
+
+Apex_39106 <- read_csv("Apex_DataLogs/20191029/39106_outlog_100519.csv",
+                       col_names = FALSE,
+                       skip=5) #skips first 5 rows containing system information
+
+#Splits data into separate columns
+Apex_39106 <- separate(Apex_39106,"X1", into=c("Record",NA), sep="</outlog>", remove=TRUE) # Removes last row </outlog>
+Apex_39106 <- separate(Apex_39106,"Record", into=c("Record","Date"), sep="<record><date>", remove=TRUE) # Splits other data from date and time
+Apex_39106 <- separate(Apex_39106,"Record",into=c("Status","Name"), sep="<name>",remove=TRUE) # Splits unit status and unit name 
+Apex_39106 <- separate(Apex_39106,"Name",into=c("Name",NA), sep="</name>",remove=TRUE) # Removes </name>
+Apex_39106 <- separate(Apex_39106,"Status",into=c("Status",NA), sep="</value>",remove=TRUE) # Removes </value></record>
+Apex_39106 <- separate(Apex_39106,"Status",into=c(NA,"Status"), sep=">",remove=TRUE) # Removes <value> and last row </outlog>
+
+# Removes NA values from Name column
+Status_col <- Apex_39106["Status"]
+Status_col <- drop_na_(Status_col,"Status")
+
+Name_col <- Apex_39106["Name"]
+Name_col <- drop_na_(Name_col,"Name")
+
+Date_col <- Apex_39106["Date"]
+Date_col <- drop_na_(Date_col,"Date")
+
+# Binds all columns together to match up data
+Apex_39106 <- cbind(Date_col,Name_col,Status_col)
+Apex_39106 <- separate(Apex_39106,"Date",into=c("Date","Time"), sep=" ",remove=TRUE) # Splits date and time
+Apex_39106 <- separate(Apex_39106,"Time",into=c("Time",NA), sep="</date>",remove=TRUE) # Removes </date>
+
+View(Apex_39106)
+write_csv(Apex_39106,"Apex_DataLogs/20191029/39106_outlog.csv")
