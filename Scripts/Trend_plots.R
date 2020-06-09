524ec4d910669693e2e3d22216643fe3c01b5302
Danielle Barnas <danielle.barnas@csun.edu>
2020-06-09 16:38:55 -0700

    updated script for removal of unrepresentative values and upload new figures

--- Scripts/Trend_plots.R
+++ Scripts/Trend_plots.R
@@ -19,27 +19,42 @@ datalog$Date<-datalog$Date%>%
   parse_datetime(format = "%Y-%m-%d %H:%M:%S", na=character(),
                  locale = default_locale(), trim_ws = TRUE)
 
-# categorize treatments
+# categorize treatments, separated by Probes
 log.Oc<-datalog%>%
   filter((Probe=="Tmp-1"|Probe=="Tmp-2"|Probe=="Tmp-17"|Probe=="Tmp-18"|Probe=="Tmp-19"|Probe=="Tmp-20"|
            Probe=="pH-1"|Probe=="pH-2"|Probe=="pH-17"|Probe=="pH-18"|Probe=="pH-19"|Probe=="pH-20"))%>% #oscillating treatment tanks
-  mutate(Treatment="Oscillating")
+  mutate(Treatment="Oscillating") #categorize probes in Oscillating treatment
 log.Low<-datalog%>%
   filter((Probe=="Tmp-3"|Probe=="Tmp-4"|Probe=="Tmp-7"|Probe=="Tmp-8"|Probe=="Tmp-9"|Probe=="Tmp-10")|
            (Probe=="pH-3"|Probe=="pH-4"|Probe=="pH-7"|Probe=="pH-8"|Probe=="pH-9"|Probe=="pH-10"))%>% #low pH treatment tanks
-  mutate(Treatment="Low")
+  mutate(Treatment="Low") #categorize probes in Low treatment
 log.Am<-datalog%>%
   filter((Probe=="Tmp-11"|Probe=="Tmp-12"|Probe=="Tmp-13"|Probe=="Tmp-14"|Probe=="Tmp-15"|Probe=="Tmp-16")|
            (Probe=="pH-11"|Probe=="pH-12"|Probe=="pH-13"|Probe=="pH-14"|Probe=="pH-15"|Probe=="pH-16"))%>% #ambient treatment tanks
-  mutate(Treatment="Ambient")
+  mutate(Treatment="Ambient") #categorize probes in Ambient treatment
 
 # bind all rows to combine treatment data, summarise data by Date, Type, and Treatment, and filter by date
 datalog<-log.Oc%>%
-  rbind(log.Low,log.Am)%>%
+  rbind(log.Low,log.Am)%>% # bind all treatment data
+  arrange(Date)%>% # rearrange by Date and time
+  filter((Date>="2020-02-05 6:00") & (Date<="2020-02-20")) # filter out "test time" data
+
+# Add column for difference in value from previous recorded value
+datalog<-datalog%>%
+  group_by(Type,Probe)%>%
   arrange(Date)%>%
+  mutate(Delta=Value-lag(Value,default=first(Value)))
+
+# isolate and remove outliers (off-values during calibrations or other non-representative data)
+outlier<-datalog%>%
+  filter(Type=="pH")%>%
+  filter((Delta>=0.15)|(Delta<=(-0.15))) # filter extreme values
+outlier # view head of dataframe
+datalog<-datalog%>%
+  anti_join(outlier, by=c("Date","Probe"))%>% #remove outliers
   group_by(Date,Type,Treatment)%>%
-  summarise(mean=mean(Value),SE=std.error(Value))%>%
-  filter((Date>="2020-02-05 6:00") & (Date<="2020-02-20")) # filter by dates
+  summarise(mean=mean(Value),SE=std.error(Value)) #create columns for mean and standard error of remaining data
+datalog # view head of dataframe
 
 # separate temp and pH data
 tempdata<-datalog%>%
@@ -50,29 +65,19 @@ phdata<-datalog%>%
 # All plots
 plot<-ggplot(data=datalog, aes(x=Date, y=mean, colour=Treatment))+
   geom_line(aes(colour=Treatment))+
-  geom_errorbar(aes(ymin=mean-SE,ymax=mean+SE),width=0.2,
-                position=position_dodge(0.05))+
+  geom_errorbar(aes(ymin=mean-SE,ymax=mean+SE),width=0.2,position=position_dodge(0.05))+
   facet_wrap(ncol=1,~Treatment+Type, scales="free_y")+
   ggsave("Output/20200607/plot_error.png")
-# Temp plot
+# Temp plots
 Tempplot<-ggplot(data=tempdata, aes(x=Date, y=mean, colour=Treatment))+
   geom_line(aes(colour=Treatment))+
-  geom_errorbar(aes(ymin=mean-SE,ymax=mean+SE),width=0.2,
-                position=position_dodge(0.05))+
+  geom_errorbar(aes(ymin=mean-SE,ymax=mean+SE),width=0.2,position=position_dodge(0.05))+
   facet_wrap(ncol=1,~Treatment+Type, scales="free_y")+
   ggsave("Output/20200607/tempplot_error.png")
-# pH plot
+# pH plots
 pHplot<-ggplot(data=phdata, aes(x=Date, y=mean, colour=Treatment))+
   geom_line(aes(colour=Treatment))+
-  geom_errorbar(aes(ymin=mean-SE,ymax=mean+SE),width=0.2,
-                position=position_dodge(0.05))+
+  geom_errorbar(aes(ymin=mean-SE,ymax=mean+SE),width=0.2,position=position_dodge(0.05))+
   facet_wrap(ncol=1,~Treatment+Type, scales="free_y")+
   ggsave("Output/20200607/pHplot_error.png")
-
-#average time series of all tanks by treatment, 3 subplots next to each other
-#take avg and std error 
-#delete wonky data prior to feb 1 ish
-
-#view outliers
-datalog%>%
-  filter((Type=="Temp")&(Treatment=="Oscillating")&(Value>17.2))
+plot # plot graphs
