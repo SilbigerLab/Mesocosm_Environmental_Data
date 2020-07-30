9d3b1699c24ea0e1fa517ac8b3a7095b33a803fa
Danielle Barnas <danielle.barnas@csun.edu>
2020-07-30 10:53:32 -0700

    add 200725 apex_1 datalog

--- Scripts/Datalog_Data_Tidy.R
+++ Scripts/Datalog_Data_Tidy.R
@@ -7,10 +7,10 @@ library(tidyverse)
 ########################
 # File Names
 ########################
-foldername<-'20200209' # folder of the day
-date<-'20200209' # today's date
+foldername<-'20200725' # folder of the day
+date<-'20200725' # today's date
 Apex_All_Datalogs<-'Apex_Full_Datalog.csv' # Larger data set your compiled new data will be added to
-Apex_1_filename<-'39106_200216d4.csv' # data from Apex 39106 : year, month, day, # of days to record after log start
+Apex_1_filename<-'datalog_1_200721d4.csv' # data from Apex 39106 : year, month, day, # of days to record after log start
 Apex_2_filename<-'40216_200216d4.csv' # data from Apex 40216
 Apex_3_filename<-'39952_200216d4.csv' # data from Apex 39952
 Apex_4_filename<-'37810_200216d4.csv' # data from Apex 37810
@@ -26,7 +26,7 @@ Apex_5_filename<-'41239_200216d4.csv' # data from Apex 41239
 Apex_39106 <- read_csv(paste0('Data/Apex_DataLogs/',foldername,'/',Apex_1_filename),
                        col_names = FALSE,
                        skip=6) #skips first 6 rows containing system information
-
+View(Apex_39106)
 #Splits data into separate columns
 Apex_39106 <- separate(Apex_39106,"X1", into=c("Date",NA), sep="</date>", remove=TRUE) # Removes </date>
 Apex_39106 <- separate(Apex_39106,"Date", into=c("Date",NA), sep="<probe>", remove=TRUE) # Removes <probe>
@@ -39,7 +39,7 @@ Apex_39106 <- separate(Apex_39106,"Probe",into=c("Probe","Type"), sep="</name> <
 Apex_39106 <- separate(Apex_39106,"Type",into=c("Type","Value"), sep="</type><value>",remove=TRUE) # Splits probe type from probe value
 Apex_39106 <- separate(Apex_39106,"Value",into=c("Value",NA), sep="</value></probe>",remove=TRUE) # Removes <probe>
 
-Apex_39106 <- separate(Apex_39106,"Date", into=c(NA,"Date"), sep="<date>", remove=TRUE)
+Apex_39106 <- separate(Apex_39106,"Date", into=c(NA,"Date"), sep="<date>", remove=TRUE) # removes <date>
 Apex_39106 <- fill(Apex_39106,"Date",.direction = c("down")) # Fills in date/time data for all probes/types/values until reaches the next date/time
 
 # Removes all blank columns (values == NA)
@@ -57,7 +57,7 @@ Apex_39106$Value <- Apex_39106$Value %>%
 # Optional: separate date and time
 #Apex_39106 <- separate(Apex_39106,"Date", into=c("Date","Time"), sep=" ", remove=TRUE)
 
-# For bringing in multiple files for the same apex in the same folder
+# For bringing in multiple files for the same apex in the same folder (if volume of apex data requires multiple smaller files)
 If(paste0('Data/Apex_DataLogs/',foldername,'/','39106_datalog.csv')=TRUE){
   Apex_39106_union <- read_csv(paste0('Data/Apex_DataLogs/',foldername,'/','39106_datalog.csv'),
                                col_names = TRUE)
@@ -89,7 +89,7 @@ Apex_40216 <- separate(Apex_40216,"Probe",into=c("Probe","Type"), sep="</name> <
 Apex_40216 <- separate(Apex_40216,"Type",into=c("Type","Value"), sep="</type><value>",remove=TRUE) # Splits probe type from probe value
 Apex_40216 <- separate(Apex_40216,"Value",into=c("Value",NA), sep="</value></probe>",remove=TRUE) # Removes <probe>
 
-Apex_40216 <- separate(Apex_40216,"Date", into=c(NA,"Date"), sep="<date>", remove=TRUE)
+Apex_40216 <- separate(Apex_40216,"Date", into=c(NA,"Date"), sep="<date>", remove=TRUE) # removes <date>
 Apex_40216 <- fill(Apex_40216,"Date",.direction = c("down")) # Fills in date/time data for all probes/types/values until reaches the next date/time
 
 # Removes all blank columns (values == NA)
@@ -107,7 +107,7 @@ Apex_40216$Value <- Apex_40216$Value %>%
 # Optional: separate date and time
 #Apex_40216 <- separate(Apex_40216,"Date", into=c("Date","Time"), sep=" ", remove=TRUE)
 
-# For bringing in multiple files for the same apex in the same folder
+# For bringing in multiple files for the same apex in the same folder (if volume of apex data requires multiple smaller files)
 If(paste0('Data/Apex_DataLogs/',foldername,'/','40216_datalog.csv')=TRUE){
   Apex_40216_union <- read_csv(paste0('Data/Apex_DataLogs/',foldername,'/','40216_datalog.csv'),
                          col_names = TRUE)
@@ -139,7 +139,7 @@ Apex_39952 <- separate(Apex_39952,"Probe",into=c("Probe","Type"), sep="</name> <
 Apex_39952 <- separate(Apex_39952,"Type",into=c("Type","Value"), sep="</type><value>",remove=TRUE) # Splits probe type from probe value
 Apex_39952 <- separate(Apex_39952,"Value",into=c("Value",NA), sep="</value></probe>",remove=TRUE) # Removes <probe>
 
-Apex_39952 <- separate(Apex_39952,"Date", into=c(NA,"Date"), sep="<date>", remove=TRUE)
+Apex_39952 <- separate(Apex_39952,"Date", into=c(NA,"Date"), sep="<date>", remove=TRUE) # removes <date>
 Apex_39952 <- fill(Apex_39952,"Date",.direction = c("down")) # Fills in date/time data for all probes/types/values until reaches the next date/time
 
 # Removes all blank columns (values == NA)
@@ -157,7 +157,7 @@ Apex_39952$Value <- Apex_39952$Value %>%
 # Optional: separate date and time
 #Apex_39952 <- separate(Apex_39952,"Date", into=c("Date","Time"), sep=" ", remove=TRUE)
 
-# For bringing in multiple files for the same apex in the same folder
+# For bringing in multiple files for the same apex in the same folder (if volume of apex data requires multiple smaller files)
 If(paste0('Data/Apex_DataLogs/',foldername,'/','39952_datalog.csv')=TRUE){
   Apex_39952_union <- read_csv(paste0('Data/Apex_DataLogs/',foldername,'/','39952_datalog.csv'),
                                col_names = TRUE)
@@ -189,7 +189,7 @@ Apex_37810 <- separate(Apex_37810,"Probe",into=c("Probe","Type"), sep="</name> <
 Apex_37810 <- separate(Apex_37810,"Type",into=c("Type","Value"), sep="</type><value>",remove=TRUE) # Splits probe type from probe value
 Apex_37810 <- separate(Apex_37810,"Value",into=c("Value",NA), sep="</value></probe>",remove=TRUE) # Removes <probe>
 
-Apex_37810 <- separate(Apex_37810,"Date", into=c(NA,"Date"), sep="<date>", remove=TRUE)
+Apex_37810 <- separate(Apex_37810,"Date", into=c(NA,"Date"), sep="<date>", remove=TRUE) # removes <date>
 Apex_37810 <- fill(Apex_37810,"Date",.direction = c("down")) # Fills in date/time data for all probes/types/values until reaches the next date/time
 
 # Removes all blank columns (values == NA)
@@ -207,7 +207,7 @@ Apex_37810$Value <- Apex_37810$Value %>%
 # Optional: separate date and time
 #Apex_37810 <- separate(Apex_37810,"Date", into=c("Date","Time"), sep=" ", remove=TRUE)
 
-# For bringing in multiple files for the same apex in the same folder
+# For bringing in multiple files for the same apex in the same folder (if volume of apex data requires multiple smaller files)
 If(paste0('Data/Apex_DataLogs/',foldername,'/','37810_datalog.csv')=TRUE){
   Apex_37810_union <- read_csv(paste0('Data/Apex_DataLogs/',foldername,'/','37810_datalog.csv'),
                                col_names = TRUE)
@@ -239,7 +239,7 @@ Apex_41239 <- separate(Apex_41239,"Probe",into=c("Probe","Type"), sep="</name> <
 Apex_41239 <- separate(Apex_41239,"Type",into=c("Type","Value"), sep="</type><value>",remove=TRUE) # Splits probe type from probe value
 Apex_41239 <- separate(Apex_41239,"Value",into=c("Value",NA), sep="</value></probe>",remove=TRUE) # Removes <probe>
 
-Apex_41239 <- separate(Apex_41239,"Date", into=c(NA,"Date"), sep="<date>", remove=TRUE)
+Apex_41239 <- separate(Apex_41239,"Date", into=c(NA,"Date"), sep="<date>", remove=TRUE) # removes <date>
 Apex_41239 <- fill(Apex_41239,"Date",.direction = c("down")) # Fills in date/time data for all probes/types/values until reaches the next date/time
 
 # Removes all blank columns (values == NA)
@@ -257,7 +257,7 @@ Apex_41239$Value <- Apex_41239$Value %>%
 # Optional: separate date and time
 #Apex_41239 <- separate(Apex_41239,"Date", into=c("Date","Time"), sep=" ", remove=TRUE)
 
-# For bringing in multiple files for the same apex in the same folder
+# For bringing in multiple files for the same apex in the same folder (if volume of apex data requires multiple smaller files)
 If(paste0('Data/Apex_DataLogs/',foldername,'/','41239_datalog.csv')=TRUE){
   Apex_41239_union <- read_csv(paste0('Data/Apex_DataLogs/',foldername,'/','41239_datalog.csv'),
                                col_names = TRUE)
