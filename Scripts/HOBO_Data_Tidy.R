d3c57d0da4177cf559411e2edcfd2d9f2c1f83dc
Danielle Barnas <danielle.barnas@csun.edu>
2020-08-05 13:07:36 -0700

    updated script for easier hobo data tidying

--- Scripts/HOBO_Data_Tidy.R
+++ Scripts/HOBO_Data_Tidy.R
@@ -6,26 +6,27 @@ foldername<-'Data/HOBO_loggers/20200803/'
 folder_date<-'20200803'
 
 # filenames for each tank
-Tank1 <- 'TNK-1-SN20569922 2020-08-04 12_51_33 -0700.csv'
-Tank2 <- 'TNK-2-SN20565250 2020-08-04 12_51_59 -0700.csv'
-Tank3 <- 'TNK-3-SN20565253 2020-08-04 12_52_27 -0700.csv'
-Tank4 <- 'TNK-4-SN20565252 2020-08-04 12_52_52 -0700.csv'
-Tank5 <- 'TNK-5-SN20555838 2020-08-04 12_56_16 -0700.csv'
-Tank6 <- 'TNK-6-20565254 2020-08-04 12_57_32 -0700.csv'
-Tank7 <- 'TNK-7-SN20565255 2020-08-04 12_59_02 -0700.csv'
-Tank8 <- 'TNK-8-SN20714139 2020-08-04 12_59_35 -0700.csv'
-Tank9 <- 'TNK-9-SN20565257 2020-08-04 13_02_51 -0700.csv'
-Tank10 <- 'TNK-10-SN20565258 2020-08-04 13_03_17 -0700.csv'
-Tank11 <- 'TNK-11-SN20565259 2020-08-04 13_03_55 -0700.csv'
-Tank12 <- 'TNK-12-SN20714140 2020-08-04 13_04_25 -0700.csv'
-Tank13 <- 'TNK-13-SN20565261 2020-08-04 13_07_03 -0700.csv'
-Tank14 <- 'TNK-14-SN20565262 2020-08-04 13_07_28 -0700.csv'
-Tank15 <- 'TNK-15-SN20565263 2020-08-04 13_07_56 -0700.csv'
-Tank16 <- 'TNK-16-SN20714141 2020-08-04 13_08_21 -0700.csv'
-Tank17 <- 'TNK-17-SN20565265 2020-08-04 13_10_36 -0700.csv'
-Tank18 <- 'TNK-18-SN20569983 2020-08-04 13_11_04 -0700.csv'
-Tank19 <- 'TNK-19-SN20714142 2020-08-04 13_11_45 -0700.csv'
-Tank20 <- 'TNK-20-SN20714143 2020-08-04 13_12_19 -0700.csv' 
+# comment out any tanks for which you have no data
+Tank1 <- 'TNK-1-SN20569922 2020-03-16 17_27_02 -0700.csv'
+Tank2 <- 'TNK-2-SN20565250 2020-03-16 17_18_46 -0700.csv'
+Tank3 <- 'TNK-3-SN20565253 2020-03-16 17_19_53 -0700.csv'
+Tank4 <- 'TNK-4-SN20565252 2020-03-16 17_22_43 -0700.csv'
+Tank5 <- 'TNK-5-SN20555838 2020-07-31 14_47_33 -0700.csv'
+Tank6 <- 'TNK-6-SN20565254 2020-03-16 17_18_29 -0700.csv'
+Tank7 <- 'TNK-7-SN20565255 2020-03-16 17_21_00 -0700.csv'
+Tank8 <- 'TNK-8-SN20714139 2020-03-16 17_22_12 -0700.csv'
+Tank9 <- 'TNK-9-SN20565257 2020-03-16 17_17_30 -0700.csv'
+Tank10 <- 'TNK-10-SN20565258 2020-03-16 17_23_14 -0700.csv'
+Tank11 <- 'TNK-11-SN20565259 2020-03-16 17_21_50 -0700.csv'
+Tank12 <- 'TNK-12-SN20714140 2020-03-16 17_19_33 -0700.csv'
+Tank13 <- 'TNK-13-SN20565261 2020-03-16 17_17_08 -0700.csv'
+Tank14 <- 'TNK-14-SN20565262 2020-03-16 17_26_36 -0700.csv'
+Tank15 <- 'TNK-15-SN20565263 2020-03-16 17_27_46 -0700.csv'
+Tank16 <- 'TNK-16-SN20714141 2020-03-16 17_17_52 -0700.csv'
+Tank17 <- 'TNK-17-SN20565265 2020-03-16 17_21_30 -0700.csv'
+Tank18 <- 'TNK-18-SN20569983 2020-03-16 17_19_10 -0700.csv'
+Tank19 <- 'TNK-19-SN20714142 2020-03-16 17_18_10 -0700.csv'
+Tank20 <- 'TNK-20-SN20714143 2020-03-16 17_20_15 -0700.csv' 
 # If not reading in data for Tank 20, Run script up to Merge section, then follow instructions
 
 ########################################################
@@ -40,7 +41,7 @@ Tnk1 <- read_csv(paste0(foldername,Tank1),
                                                "Host Connect"=col_skip(),"Stopped"=col_skip(),"EOF"=col_skip()))
 Tnk1 <- drop_na(Tnk1)
 Tnk1 <- Tnk1 %>%
-  rename(Tnk1_TmpC = `Temp, (*C)`)
+  rename(Tnk1_TmpC = `Temp, (*C)`,Date="Date Time, GMT -0700")
 }
 If(exists(Tank2)) {
 Tnk2 <- read_csv(paste0(foldername,Tank2),
@@ -50,7 +51,7 @@ Tnk2 <- read_csv(paste0(foldername,Tank2),
                                                "Host Connect"=col_skip(),"Stopped"=col_skip(),"EOF"=col_skip()))
 Tnk2 <- drop_na(Tnk2)
 Tnk2 <- Tnk2 %>%
-  rename(Tnk2_TmpC = `Temp, (*C)`)
+  rename(Tnk2_TmpC = `Temp, (*C)`,Date="Date Time, GMT -0700")
 }
 If(exists(Tank3)) {
 Tnk3 <- read_csv(paste0(foldername,Tank3),
@@ -60,7 +61,7 @@ Tnk3 <- read_csv(paste0(foldername,Tank3),
                                                 "Host Connect"=col_skip(),"Stopped"=col_skip(),"EOF"=col_skip()))
 Tnk3 <- drop_na(Tnk3)
 Tnk3 <- Tnk3 %>%
-  rename(Tnk3_TmpC = `Temp, (*C)`)
+  rename(Tnk3_TmpC = `Temp, (*C)`,Date="Date Time, GMT -0700")
 }
 If(exists(Tank4)) {
 Tnk4 <- read_csv(paste0(foldername,Tank4),
@@ -70,7 +71,7 @@ Tnk4 <- read_csv(paste0(foldername,Tank4),
                                                "Host Connect"=col_skip(),"Stopped"=col_skip(),"EOF"=col_skip()))
 Tnk4 <- drop_na(Tnk4)
 Tnk4 <- Tnk4 %>%
-  rename(Tnk4_TmpC = `Temp, (*C)`)
+  rename(Tnk4_TmpC = `Temp, (*C)`,Date="Date Time, GMT -0700")
 }
 If(exists(Tank5)) {
 Tnk5 <- read_csv(paste0(foldername,Tank5),
@@ -80,7 +81,7 @@ Tnk5 <- read_csv(paste0(foldername,Tank5),
                                                "Host Connect"=col_skip(),"Stopped"=col_skip(),"EOF"=col_skip()))
 Tnk5 <- drop_na(Tnk5)
 Tnk5 <- Tnk5 %>%
-  rename(Tnk5_TmpC = `Temp, (*C)`)
+  rename(Tnk5_TmpC = `Temp, (*C)`,Date="Date Time, GMT -0700")
 }
 If(exists(Tank6)) {
 Tnk6 <- read_csv(paste0(foldername,Tank6),
@@ -90,7 +91,7 @@ Tnk6 <- read_csv(paste0(foldername,Tank6),
                                                "Host Connect"=col_skip(),"Stopped"=col_skip(),"EOF"=col_skip()))
 Tnk6 <- drop_na(Tnk6)
 Tnk6 <- Tnk6 %>%
-  rename(Tnk6_TmpC = `Temp, (*C)`)
+  rename(Tnk6_TmpC = `Temp, (*C)`,Date="Date Time, GMT -0700")
 }
 If(exists(Tank7)) {
 Tnk7 <- read_csv(paste0(foldername,Tank7),
@@ -100,7 +101,7 @@ Tnk7 <- read_csv(paste0(foldername,Tank7),
                                                "Host Connect"=col_skip(),"Stopped"=col_skip(),"EOF"=col_skip()))
 Tnk7 <- drop_na(Tnk7)
 Tnk7 <- Tnk7 %>%
-  rename(Tnk7_TmpC = `Temp, (*C)`)
+  rename(Tnk7_TmpC = `Temp, (*C)`,Date="Date Time, GMT -0700")
 }
 If(exists(Tank8)) {
 Tnk8 <- read_csv(paste0(foldername,Tank8),
@@ -110,7 +111,7 @@ Tnk8 <- read_csv(paste0(foldername,Tank8),
                                                 "Host Connect"=col_skip(),"Stopped"=col_skip(),"EOF"=col_skip()))
 Tnk8 <- drop_na(Tnk8)
 Tnk8 <- Tnk8 %>%
-  rename(Tnk8_TmpC = `Temp, (*C)`)
+  rename(Tnk8_TmpC = `Temp, (*C)`,Date="Date Time, GMT -0700")
 }
 If(exists(Tank9)) {
 Tnk9 <- read_csv(paste0(foldername,Tank9),
@@ -120,7 +121,7 @@ Tnk9 <- read_csv(paste0(foldername,Tank9),
                                                "Host Connect"=col_skip(),"Stopped"=col_skip(),"EOF"=col_skip()))
 Tnk9 <- drop_na(Tnk9)
 Tnk9 <- Tnk9 %>%
-  rename(Tnk9_TmpC = `Temp, (*C)`)
+  rename(Tnk9_TmpC = `Temp, (*C)`,Date="Date Time, GMT -0700")
 }
 If(exists(Tank10)) {
 Tnk10 <- read_csv(paste0(foldername,Tank10),
@@ -130,7 +131,7 @@ Tnk10 <- read_csv(paste0(foldername,Tank10),
                                                 "Host Connect"=col_skip(),"Stopped"=col_skip(),"EOF"=col_skip()))
 Tnk10 <- drop_na(Tnk10)
 Tnk10 <- Tnk10 %>%
-  rename(Tnk10_TmpC = `Temp, (*C)`)
+  rename(Tnk10_TmpC = `Temp, (*C)`,Date="Date Time, GMT -0700")
 }
 If(exists(Tank11)) {
 Tnk11 <- read_csv(paste0(foldername,Tank11),
@@ -140,7 +141,7 @@ Tnk11 <- read_csv(paste0(foldername,Tank11),
                                                 "Host Connect"=col_skip(),"Stopped"=col_skip(),"EOF"=col_skip()))
 Tnk11 <- drop_na(Tnk11)
 Tnk11 <- Tnk11 %>%
-  rename(Tnk11_TmpC = `Temp, (*C)`)
+  rename(Tnk11_TmpC = `Temp, (*C)`,Date="Date Time, GMT -0700")
 }
 If(exists(Tank12)) {
 Tnk12 <- read_csv(paste0(foldername,Tank12),
@@ -150,7 +151,7 @@ Tnk12 <- read_csv(paste0(foldername,Tank12),
                                                 "Host Connect"=col_skip(),"Stopped"=col_skip(),"EOF"=col_skip()))
 Tnk12 <- drop_na(Tnk12)
 Tnk12 <- Tnk12 %>%
-  rename(Tnk12_TmpC = `Temp, (*C)`)
+  rename(Tnk12_TmpC = `Temp, (*C)`,Date="Date Time, GMT -0700")
 }
 If(exists(Tank13)) {
 Tnk13 <- read_csv(paste0(foldername,Tank13),
@@ -160,7 +161,7 @@ Tnk13 <- read_csv(paste0(foldername,Tank13),
                                                 "Host Connect"=col_skip(),"Stopped"=col_skip(),"EOF"=col_skip()))
 Tnk13 <- drop_na(Tnk13)
 Tnk13 <- Tnk13 %>%
-  rename(Tnk13_TmpC = `Temp, (*C)`)
+  rename(Tnk13_TmpC = `Temp, (*C)`,Date="Date Time, GMT -0700")
 }
 If(exists(Tank14)) {
 Tnk14 <- read_csv(paste0(foldername,Tank14),
@@ -170,7 +171,7 @@ Tnk14 <- read_csv(paste0(foldername,Tank14),
                                                 "Host Connect"=col_skip(),"Stopped"=col_skip(),"EOF"=col_skip()))
 Tnk14 <- drop_na(Tnk14)
 Tnk14 <- Tnk14 %>%
-  rename(Tnk14_TmpC = `Temp, (*C)`)
+  rename(Tnk14_TmpC = `Temp, (*C)`,Date="Date Time, GMT -0700")
 }
 If(exists(Tank15)) {
 Tnk15 <- read_csv(paste0(foldername,Tank15),
@@ -180,7 +181,7 @@ Tnk15 <- read_csv(paste0(foldername,Tank15),
                                                 "Host Connect"=col_skip(),"Stopped"=col_skip(),"EOF"=col_skip()))
 Tnk15 <- drop_na(Tnk15)
 Tnk15 <- Tnk15 %>%
-  rename(Tnk15_TmpC = `Temp, (*C)`)
+  rename(Tnk15_TmpC = `Temp, (*C)`,Date="Date Time, GMT -0700")
 }
 If(exists(Tank16)) {
 Tnk16 <- read_csv(paste0(foldername,Tank16),
@@ -190,7 +191,7 @@ Tnk16 <- read_csv(paste0(foldername,Tank16),
                                                 "Host Connect"=col_skip(),"Stopped"=col_skip(),"EOF"=col_skip()))
 Tnk16 <- drop_na(Tnk16)
 Tnk16 <- Tnk16 %>%
-  rename(Tnk16_TmpC = `Temp, (*C)`)
+  rename(Tnk16_TmpC = `Temp, (*C)`,Date="Date Time, GMT -0700")
 }
 If(exists(Tank17)) {
 Tnk17 <- read_csv(paste0(foldername,Tank17),
@@ -200,7 +201,7 @@ Tnk17 <- read_csv(paste0(foldername,Tank17),
                                                 "Host Connect"=col_skip(),"Stopped"=col_skip(),"EOF"=col_skip()))
 Tnk17 <- drop_na(Tnk17)
 Tnk17 <- Tnk17 %>%
-  rename(Tnk17_TmpC = `Temp, (*C)`)
+  rename(Tnk17_TmpC = `Temp, (*C)`,Date="Date Time, GMT -0700")
 }
 If(exists(Tank18)) {
   Tnk18 <- read_csv(paste0(foldername,Tank18),
@@ -210,7 +211,7 @@ If(exists(Tank18)) {
                                                 "Host Connect"=col_skip(),"Stopped"=col_skip(),"EOF"=col_skip()))
 Tnk18 <- drop_na(Tnk18)
 Tnk18 <- Tnk18 %>%
-  rename(Tnk18_TmpC = `Temp, (*C)`)
+  rename(Tnk18_TmpC = `Temp, (*C)`,Date="Date Time, GMT -0700")
 }
 If(exists(Tank19)) {
 Tnk19 <- read_csv(paste0(foldername,Tank19),
@@ -220,7 +221,7 @@ Tnk19 <- read_csv(paste0(foldername,Tank19),
                                                          "Host Connect"=col_skip(),"Stopped"=col_skip(),"EOF"=col_skip()))
 Tnk19 <- drop_na(Tnk19)
 Tnk19 <- Tnk19 %>%
-  rename(Tnk19_TmpC = `Temp, (*C)`)
+  rename(Tnk19_TmpC = `Temp, (*C)`,Date="Date Time, GMT -0700")
 }
 If(exists(Tank20)) {
 Tnk20 <- read_csv(paste0(foldername,Tank20),
@@ -230,78 +231,79 @@ Tnk20 <- read_csv(paste0(foldername,Tank20),
                                                 "Host Connect"=col_skip(),"Stopped"=col_skip(),"EOF"=col_skip()))
 Tnk20 <- drop_na(Tnk20)
 Tnk20 <- Tnk20 %>%
-  rename(Tnk20_TmpC = `Temp, (*C)`)
+  rename(Tnk20_TmpC = `Temp, (*C)`,Date="Date Time, GMT -0700")
 }
 ##################
 # Merge Data
 ##################
 
-# Merge all Temp data into one dataframe
-
 # If not using tank 20, update line 243 to assign the last tank with recorded data to Hobo_All,
 # then skip to the next merge line for the tank number AFTER the tank recorded in line 243. Run the rest of the script.
+
 If(exists(Tnk20)){
   Hobo_All <- Tnk20
   }
 If(exists(Tnk19)){
-  Hobo_All <- merge(x = Tnk19, y = Hobo_All, by = "Date Time, GMT -0700", all = TRUE)
+  Hobo_All <- merge(x = Tnk19, y = Hobo_All, by = 'Date', all = TRUE)
   }
 If(exists(Tnk18)){
-  Hobo_All <- merge(x = Tnk18, y = Hobo_All, by = "Date Time, GMT -0700", all = TRUE)
+  Hobo_All <- merge(x = Tnk18, y = Hobo_All, by = 'Date', all = TRUE)
   }
 If(exists(Tnk17)){
-  Hobo_All <- merge(x = Tnk17, y = Hobo_All, by = "Date Time, GMT -0700", all = TRUE)
+  Hobo_All <- merge(x = Tnk17, y = Hobo_All, by = 'Date', all = TRUE)
   }
 If(exists(Tnk16)){
-  Hobo_All <- merge(x = Tnk16, y = Hobo_All, by = "Date Time, GMT -0700", all = TRUE)
+  Hobo_All <- merge(x = Tnk16, y = Hobo_All, by = 'Date', all = TRUE)
 }
 If(exists(Tnk15)){
-  Hobo_All <- merge(x = Tnk15, y = Hobo_All, by = "Date Time, GMT -0700", all = TRUE)
+  Hobo_All <- merge(x = Tnk15, y = Hobo_All, by = 'Date', all = TRUE)
   }
-If(exists(Tnk14)){Hobo_All <- merge(x = Tnk14, y = Hobo_All, by = "Date Time, GMT -0700", all = TRUE)
+If(exists(Tnk14)){
+  Hobo_All <- merge(x = Tnk14, y = Hobo_All, by = 'Date', all = TRUE)
 }
 If(exists(Tnk13)){
-  Hobo_All <- merge(x = Tnk13, y = Hobo_All, by = "Date Time, GMT -0700", all = TRUE)
+  Hobo_All <- merge(x = Tnk13, y = Hobo_All, by = 'Date', all = TRUE)
   }
 If(exists(Tnk12)){
-  Hobo_All <- merge(x = Tnk12, y = Hobo_All, by = "Date Time, GMT -0700", all = TRUE)
+  Hobo_All <- merge(x = Tnk12, y = Hobo_All, by = 'Date', all = TRUE)
   }
 If(exists(Tnk11)){
-  Hobo_All <- merge(x = Tnk11, y = Hobo_All, by = "Date Time, GMT -0700", all = TRUE)
+  Hobo_All <- merge(x = Tnk11, y = Hobo_All, by = 'Date', all = TRUE)
   }
 If(exists(Tnk10)){
-  Hobo_All <- merge(x = Tnk10, y = Hobo_All, by = "Date Time, GMT -0700", all = TRUE)
+  Hobo_All <- merge(x = Tnk10, y = Hobo_All, by = 'Date', all = TRUE)
   }
 If(exists(Tnk9)){
-  Hobo_All <- merge(x = Tnk9, y = Hobo_All, by = "Date Time, GMT -0700", all = TRUE)
+  Hobo_All <- merge(x = Tnk9, y = Hobo_All, by = 'Date', all = TRUE)
   }
 If(exists(Tnk8)){
-  Hobo_All <- merge(x = Tnk8, y = Hobo_All, by = "Date Time, GMT -0700", all = TRUE)
+  Hobo_All <- merge(x = Tnk8, y = Hobo_All, by = 'Date', all = TRUE)
   }
 If(exists(Tnk7)){
-  Hobo_All <- merge(x = Tnk7, y = Hobo_All, by = "Date Time, GMT -0700", all = TRUE)
+  Hobo_All <- merge(x = Tnk7, y = Hobo_All, by = 'Date', all = TRUE)
   }
 If(exists(Tnk6)){
-  Hobo_All <- merge(x = Tnk6, y = Hobo_All, by = "Date Time, GMT -0700", all = TRUE)
+  Hobo_All <- merge(x = Tnk6, y = Hobo_All, by = 'Date', all = TRUE)
   }
 If(exists(Tnk5)){
-  Hobo_All <- merge(x = Tnk5, y = Hobo_All, by = "Date Time, GMT -0700", all = TRUE)
+  Hobo_All <- merge(x = Tnk5, y = Hobo_All, by = 'Date', all = TRUE)
   }
 If(exists(Tnk4)){
-  Hobo_All <- merge(x = Tnk4, y = Hobo_All, by = "Date Time, GMT -0700", all = TRUE)
+  Hobo_All <- merge(x = Tnk4, y = Hobo_All, by = 'Date', all = TRUE)
   }
 If(exists(Tnk3)){
-  Hobo_All <- merge(x = Tnk3, y = Hobo_All, by = "Date Time, GMT -0700", all = TRUE)
+  Hobo_All <- merge(x = Tnk3, y = Hobo_All, by = 'Date', all = TRUE)
   }
 If(exists(Tnk2)){
-  Hobo_All <- merge(x = Tnk2, y = Hobo_All, by = "Date Time, GMT -0700", all = TRUE)
+  Hobo_All <- merge(x = Tnk2, y = Hobo_All, by = 'Date', all = TRUE)
   }
 If(exists(Tnk1)){
-  Hobo_All <- merge(x = Tnk1, y = Hobo_All, by = "Date Time, GMT -0700", all = TRUE)
+  Hobo_All <- merge(x = Tnk1, y = Hobo_All, by = 'Date', all = TRUE)
   }
 
 # Split Date and Time into separate columns
 #Hobo_All <- separate(Hobo_All,"Date Time, GMT -0700", into=c("Date","Time"), sep = " ", remove = TRUE)
+  
 View(Hobo_All)
 
 ############## save data file
