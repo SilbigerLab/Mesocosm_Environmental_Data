81d0cd0a8c5cce318dc0455fdb1197b6fb8dffba
Danielle Barnas <danielle.barnas@csun.edu>
2020-09-28 13:23:53 -0700

    data and graphs through 9/24/2020

--- Scripts/HOBO_Trend_plots.r
+++ Scripts/HOBO_Trend_plots.r
@@ -15,17 +15,17 @@ library(plotrix)
 #####################
 # Load data
 #####################
-folder.date<- "20200907"
+folder.date<- "20200924"
 datalog<-read_csv(paste0("Data/HOBO_loggers/Amanda_Hobo_Logs.csv"),na=(c("NA", "")))
 
 #datalog<-datalog%>%
 #  select(-c('Intensity, ( lux)'))
 
 # start date and time of data logging
-startLog<-parse_datetime("2020-08-26 00:00:00",format = "%Y-%m-%d %H:%M:%S", na=character(),
+startLog<-parse_datetime("2020-09-05 00:00:00",format = "%Y-%m-%d %H:%M:%S", na=character(),
                          locale = locale(tz = ""), trim_ws = TRUE)
 # end date and time of data logging
-endLog<-parse_datetime("2020-09-07 10:00:00",format = "%Y-%m-%d %H:%M:%S", na=character(),
+endLog<-parse_datetime("2020-09-20 15:00:00",format = "%Y-%m-%d %H:%M:%S", na=character(),
                        locale = locale(tz = ""), trim_ws = TRUE)
 
 # parse date_time to POSIXct
@@ -47,33 +47,21 @@ datalog<-datalog%>%
 # split data by treatment type
 #####################
 Kelp<-datalog%>%filter((Tank=="Hobo-Tmp-1"|Tank=="Hobo-Tmp-2"))
-Am.stable<-datalog%>% # ambient stable temperature
-  filter((Tank=="Hobo-Tmp-5"|Tank=="Hobo-Tmp-9"|Tank=="Hobo-Tmp-13"|Tank=="Hobo-Tmp-17"))%>%
-  mutate(Treatment="Ambient.Stable")
-El.stable<-datalog%>% # elevated stable temperature
-  filter((Tank=="Hobo-Tmp-6"|Tank=="Hobo-Tmp-10"|Tank=="Hobo-Tmp-14"|Tank=="Hobo-Tmp-18"))%>%
-  mutate(Treatment="Elevated.Stable")
 Am.oc<-datalog%>% # ambient temperature oscillations
-  filter((Tank=="Hobo-Tmp-3"|Tank=="Hobo-Tmp-7"|Tank=="Hobo-Tmp-11"|Tank=="Hobo-Tmp-15"|Tank=="Hobo-Tmp-19"))%>%
+  filter((Tank=="Hobo-Tmp-3"|Tank=="Hobo-Tmp-4"|Tank=="Hobo-Tmp-7"|Tank=="Hobo-Tmp-8"|Tank=="Hobo-Tmp-9"|Tank=="Hobo-Tmp-11"|Tank=="Hobo-Tmp-12"|Tank=="Hobo-Tmp-14"|Tank=="Hobo-Tmp-15"|Tank=="Hobo-Tmp-17"|Tank=="Hobo-Tmp-18"|Tank=="Hobo-Tmp-20"))%>%
   mutate(Treatment="Ambient.Oscillating")
 El.oc<-datalog%>% # elevated temperature oscillations
-  filter((Tank=="Hobo-Tmp-4"|Tank=="Hobo-Tmp-8"|Tank=="Hobo-Tmp-12"|Tank=="Hobo-Tmp-16"|Tank=="Hobo-Tmp-20"))%>%
+  filter((Tank=="Hobo-Tmp-5"|Tank=="Hobo-Tmp-6"|Tank=="Hobo-Tmp-10"|Tank=="Hobo-Tmp-13"|Tank=="Hobo-Tmp-16"|Tank=="Hobo-Tmp-19"))%>%
   mutate(Treatment="Elevated.Oscillating")
 
 # bind all rows to combine treatment data and summarise data by Date and Treatment
-datalog<-Am.stable%>%
-  rbind(El.stable,Am.oc,El.oc)%>%
+datalog<-Am.oc%>%
+  rbind(El.oc)%>%
   arrange(Date)
 meanlog<-datalog%>%
   group_by(Date,Treatment)%>%
   summarise(mean=mean(Value),SE=std.error(Value))
 
-oscilate<-Am.oc%>%
-  rbind(El.oc)%>%
-  arrange(Date)
-stable<-Am.stable%>%
-  rbind(El.stable)%>%
-  arrange(Date)
 
 #####################
 # Plotting Raw Treatment Plots
@@ -86,22 +74,12 @@ plot1<-ggplot(data=datalog, aes(x=Date, y=Value, colour=Tank))+
   labs(x="Date",y="TempC",title="Raw Hobo Temperatures per Treatment")+
   ggsave(paste0("Output/",folder.date,"/Hobo_rawValues_perTreatment_plot.png"),width=11,height=7)
 # By treatment
-plot2<-ggplot(data=Am.stable, aes(x=Date, y=Value, colour=Tank))+
-  geom_line()+
-  theme_bw()+
-  labs(x="Date",y="TempC",title="Raw Hobo Temperatures",subtitle="Ambient Stable Treatment")+
-  ggsave(paste0("Output/",folder.date,"/Hobo_Ambient_Stable_plot.png"),width=11,height=6)
-plot3<-ggplot(data=El.stable, aes(x=Date, y=Value, colour=Tank))+
-  geom_line()+
-  theme_bw()+
-  labs(x="Date",y="TempC",title="Raw Hobo Temperatures",subtitle="Elevated Stable Treatment")+
-  ggsave(paste0("Output/",folder.date,"/Hobo_Elevated_Stable_plot.png"),width=11,height=6)
-plot4<-ggplot(data=Am.oc, aes(x=Date, y=Value, colour=Tank))+
+plot2<-ggplot(data=Am.oc, aes(x=Date, y=Value, colour=Tank))+
   geom_line()+
   theme_bw()+
   labs(x="Date",y="TempC",title="Raw Hobo Temperatures",subtitle="Ambient Oscillating Treatment")+
   ggsave(paste0("Output/",folder.date,"/Hobo_Ambient_Oscillating_plot.png"),width=11,height=6)
-plot5<-ggplot(data=El.oc, aes(x=Date, y=Value, colour=Tank))+
+plot3<-ggplot(data=El.oc, aes(x=Date, y=Value, colour=Tank))+
   geom_line()+
   theme_bw()+
   labs(x="Date",y="TempC",title="Raw Hobo Temperatures",subtitle="Elevated Oscillating Treatment")+
@@ -112,7 +90,7 @@ plot5<-ggplot(data=El.oc, aes(x=Date, y=Value, colour=Tank))+
 # Plotting Mean Treatment Plots
 #####################
 # Without Error Bars
-plot6<-ggplot(data=meanlog, aes(x=Date, y=mean, colour=Treatment))+
+plot4<-ggplot(data=meanlog, aes(x=Date, y=mean, colour=Treatment))+
   geom_line()+
   theme_bw()+
   facet_wrap(ncol=1,~Treatment, scales="free_y")+
@@ -120,31 +98,12 @@ plot6<-ggplot(data=meanlog, aes(x=Date, y=mean, colour=Treatment))+
        title="Mean Hobo Temperatures per Treatment",subtitle="Without Standard Error")+
   ggsave(paste0("Output/",folder.date,"/Hobo_meanValues_perTreatment_noSE_plot.png"),width=11,height=7)
 # With Error Bars
-plot7<-ggplot(data=meanlog, aes(x=Date, y=mean, colour=Treatment))+
+plot5<-ggplot(data=meanlog, aes(x=Date, y=mean, colour=Treatment))+
   geom_line()+
   theme_bw()+
   geom_errorbar(aes(ymin=mean-SE,ymax=mean+SE),width=0.2,position="identity")+
   facet_wrap(ncol=1,~Treatment, scales="free_y")+
   labs(x="Date",y="Mean TempC",title="Mean Hobo Temperatures per Treatment",subtitle="With Standard Error")+
   ggsave(paste0("Output/",folder.date,"/Hobo_meanValues_perTreatment_SE_plot.png"),width=11,height=7)
-
-
-#####################
-# Plotting Oscillating or Stable
-#####################
-# Oscilating treatments
-plot8<-ggplot(data=oscilate, aes(x=Date, y=Value, colour=Tank))+
-  geom_line()+
-  theme_bw()+
-  facet_wrap(ncol=1,~Treatment, scales="fixed")+
-  labs(x="Date",y="TempC",title="Raw Hobo Oscillating Treatment Temperatures")+
-  ggsave(paste0("Output/",folder.date,"/Hobo_rawValues_Oscilating_plot.png"),width=11,height=7)
-# Stable treatments
-plot9<-ggplot(data=stable, aes(x=Date, y=Value, colour=Tank))+
-  geom_line()+
-  theme_bw()+
-  facet_wrap(ncol=1,~Treatment, scales="fixed")+
-  labs(x="Date",y="TempC",title="Raw Hobo Stable Treatment Temperatures")+
-  ggsave(paste0("Output/",folder.date,"/Hobo_rawValues_Stable_plot.png"),width=11,height=7)
 
 
